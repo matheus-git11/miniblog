6fa5a8b939348244993b420877ba28eba2c92db8
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-08-18 00:24:05 -0300

    aplicando o react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 import './App.css';
+import {BrowserRouter, Routes , Route , Navigate} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-     <h1>miniblog</h1>
+    <BrowserRouter>
+      <div className="container">
+      <Routes>
+      
+      </Routes>
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
