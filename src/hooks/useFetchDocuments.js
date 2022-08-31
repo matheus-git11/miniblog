64dfb64b826458605a6e3c99df55724bfa02f4ca
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-08-31 15:09:23 -0300

    comecando a pagina home e iniciando usefetchDocument

---
+++ src/hooks/useFetchDocuments.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+} from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    
+  },[docCollection, search , uid , cancelled])
+};
