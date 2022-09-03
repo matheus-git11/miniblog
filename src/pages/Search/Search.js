b11593ba55a3f8a15c9efbc67c7fc126871e64a5
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-09-03 02:10:05 -0300

    ajustes no css

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,6 +1,12 @@
 import React from "react";
+
+//pages
 import PostDetail from "../../components/PostDetail";
 
+//css
+import styles from './Search.module.css'
+
+//hooks
 import { Link } from "react-router-dom";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
@@ -11,8 +17,8 @@ const Search = () => {
   const { documents: posts } = useFetchDocuments("posts" , search);
 
   return (
-    <div>
-      <h2>Search</h2>
+    <div className={styles.search_container}>
+      <h2>Voce Buscou por: {search}</h2>
       <div>
         {posts && posts.length === 0 && (
           <>
