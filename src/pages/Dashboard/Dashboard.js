2760f9de8e8135b0db20179f3e9488c4a9ad55d6
Matheus Albuquerque <matheus.2020110153@unicap.br>
2022-09-04 02:53:07 -0300

    Iniciando pagina de Dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,33 @@
-import styles from './Dashboard.module.css'
+import styles from "./Dashboard.module.css";
+import { Link } from "react-router-dom";
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  //posts do usuario
+  const posts = [];
   return (
     <div>
-        <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Nao foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro Post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>tem posts!</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
